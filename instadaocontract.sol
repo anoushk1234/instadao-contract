a4854f928db939e6e5ba902da60818171f334245
Anoushk Kharangate <32778608+anoushk1234@users.noreply.github.com>
2022-01-28 13:18:25 +0530

    Update instadaocontract.sol

--- instadaocontract.sol
+++ instadaocontract.sol
@@ -8,8 +8,8 @@ import  "hardhat/console.sol";
 contract InstaDao is ERC20, Ownable {
     uint256 _totalSupply;
     uint256 _decimals;
-    
-    constructor(uint256 supply,uint256 amt,uint256 _deci) ERC20("MyToken","MTK")  {
+    // event Mint(address to, address tokenaddress,uint256 totalSupply,uint256 amt);
+    constructor(uint256 supply,uint256 amt,uint256 _deci,string memory name,string memory symbol) ERC20(name,symbol)  {
         // console.log(totalSupply())
         _decimals=_deci;
          _totalSupply=supply * 10 ** _decimals;
@@ -23,6 +23,8 @@ contract InstaDao is ERC20, Ownable {
         // console.log(totalSupply())
         require(totalSupply() + amount * 10 ** _decimals <= _totalSupply,"Exceeding supply");
         super._mint(to, amount * 10 ** _decimals );
+        // emit Mint(to,address(this),_totalSupply,amount);
+        // DaoFactory.emitTokenCreated(address())
         // _totalSupply+=amount;
     }
     
@@ -37,25 +39,27 @@ contract InstaDao is ERC20, Ownable {
 
 contract DaoFactory{
     
-    mapping(address => InstaDao) public children;
-    event tokencreated(address tokenaddress,address creator);
-    function create(uint256 supply,uint256 amt,uint256 deci) public {
-        InstaDao instaDao = new InstaDao(supply,amt,deci);
-        children[msg.sender] = instaDao;
+    // mapping(address => InstaDao) public children;
+    event tokencreated(address tokenaddress,address creator,string  name, string  symbol);
+    function create(uint256 supply,uint256 amt,uint256 deci,string memory name, string memory symbol) public {
+        InstaDao instaDao = new InstaDao(supply,amt,deci,name,symbol);
+        // children[msg.sender] = instaDao;
         instaDao.transferOwnership(msg.sender);
-        emit tokencreated(address(instaDao),msg.sender);
+        emit tokencreated(address(instaDao),msg.sender,name,symbol);
     }
-
-    function getContract()
-        public
-        view
-        returns (
-            address
-        )
-    {
-        InstaDao instaDao = children[msg.sender];
-        return (
-            address(instaDao)
-        );
-    }
+    // function emitTokenCreated(address tokenAddress,address sender) public {
+    //     emit tokencreated(tokenAddress,sender);
+    // }
+    // function getContract()
+    //     public
+    //     view
+    //     returns (
+    //         address
+    //     )
+    // {
+    //     InstaDao instaDao = children[msg.sender];
+    //     return (
+    //         address(instaDao)
+    //     );
+    // }
 }
